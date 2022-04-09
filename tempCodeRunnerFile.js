fae7d7fa5e820cf8b0c5ba9987de59336d65dc67
gogleset <choij2494@gmail.com>
2022-04-09 12:41:13 +0900

    .

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,20 +1,40 @@
-// file system 모듈을 불러온다.
 // const fs = require('fs');
+// const array = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-// fs모듈의 readFileSync 함수를 통해 동기적으로 해당 경로의 파일 전체를 읽어들인다.
-// 백준에서는 '/dev/stdin' 경로에 테스트 케이스 파일이 있다.
-// 읽어드린 정보는 toString()함수를 통해 문자열로 변환하여야 사용할 수 있다.
-// 입력 받은 문자열을 split() 함수를 통해 배열화한다. (split(' '), split('\n') 등)
-// 예) 2 3 입력시, inputData = ['2', '3']
-// const inputData1 = fs.readFileSync('/dev/stdin').toString().split(' ');
+// 그룹 단어란 단어에 존재하는 모든 문자에 대해서,
+// 각 문자가 연속해서 나타나는 경우만을 말한다. 예를 들면,
+// ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고,
+// kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만,
+// aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
 
-// options으로 인코딩을 string 자료형으로 넘기는 경우, toString을 할 필요없이 문자열 반환
-// const inputData2 = fs.readFileSync(0, 'utf8').split(' ');
-// const fs = require('fs');
-// const [A, B] = fs.readFileSync('/dev/stdin').toString().split('\n');
+// 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
 
+const fs = `1
+z
+`;
+let input = fs.toString().trim().split("\n");
 
-const fss = `baekjoonz`;
-const input = fss.toString();
-console.log(input[8]); 
-console.log(input[8].codePointAt());
+function getGroup(array) {
+  let count = 0;
+  for (let i = 1; i < array.length; i++) {
+    let arrayCount = 0;
+    let [...arr] = new Set(Array.from(array[i]));
+    for (let j = 0; j < arr.length; j++) {
+      let firstIndex = array[i].indexOf(arr[j]);
+      let lastIndex = array[i].lastIndexOf(arr[j]);
+      if (firstIndex !== lastIndex || firstIndex !== lastIndex - 1) {
+        for (let k = firstIndex; k < lastIndex + 1; k++) {
+          if (arr[j] !== array[i][k]) {
+            arrayCount++;
+          }
+        }
+      }
+    }
+    if (arrayCount > 0) {
+      count++;
+    }
+  }
+  return console.log(array[0] - count);
+}
+
+getGroup(input);
