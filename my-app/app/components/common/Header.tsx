7e7ebfb8fabffbccbff400aa8e37b0f89b30b629
choi_j <choij2494@gmail.com>
2024-05-06 09:37:12 +0900

    add: theme 변경

--- my-app/app/components/common/Header.tsx
+++ my-app/app/components/common/Header.tsx
@@ -10,7 +10,8 @@ const Header = () => {
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <Link
           href={PATH.HOME}
-          className="self-center text-xl font-semibold whitespace-nowrap dark:text-white cursor-pointer">
+          className="self-center text-xl font-semibold whitespace-nowrap dark:text-white cursor-pointer"
+        >
           gogleset's note
         </Link>
         <div className="flex gap-3">
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -6,23 +6,12 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
   variants: {
     typography: ["dark"],
   },
-  darkMode: "class",
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
   daisyui: {
-    themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    themes: ["dracula", "cupcake"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
