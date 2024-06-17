3b578b36277f4d0dc461a7c74f2f19a2ad77fbe7
최진 <gogleset@coinplug.com>
2024-06-17 14:05:08 +0900

    add: github deploy - robots.txt, sitemap

--- my-app/app/sitemap.ts
+++ my-app/app/sitemap.ts
@@ -1,24 +1,25 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const url = "https://gogleset.github.io";
   return [
     {
-      url: "https://acme.com",
+      url,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://acme.com/post",
+      url: url + "/post",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://acme.com/about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.8,
     },
+    // {
+    //   url: "https://acme.com/about",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
   ];
 }
