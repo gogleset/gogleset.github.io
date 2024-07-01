6aa5b1ff3cbc1e2f89e4fe983466a0296eedc77b
최진 <gogleset@coinplug.com>
2024-07-01 15:54:35 +0900

    fix: content styling

--- app/components/Posts/Content.tsx
+++ app/components/Posts/Content.tsx
@@ -31,7 +31,7 @@ export default function PostContent({ mdx }: PostContentProps<Frontmatter>) {
   // MDX text - can be from a local file, database, CMS, fetch, anywhere...
   const { content } = mdx;
   return (
-    <div className="flex flex-col prose max-w-full max-sm:prose-sm">
+    <div className="flex flex-col prose max-w-full max-sm:prose-sm ">
       {content}
     </div>
   );
