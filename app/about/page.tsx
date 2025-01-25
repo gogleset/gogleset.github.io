547a3448dfd3d13d7ce8a0388a36aa19596b66e8
gogleset <choij2494@gmail.com>
2025-01-25 12:38:46 +0900

    blog 재조정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import React from "react";
 import METADATA from "../constant/metadata";
-const { title, description, keywords, openGraph } = METADATA;
+const { keywords, openGraph } = METADATA;
 
 // export const dynamic = "force-static";
 export const metadata: Metadata = {
@@ -41,7 +41,8 @@ const page = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="text-primary h-5 w-5">
+              className="text-primary h-5 w-5"
+            >
               <path
                 fillRule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -64,7 +65,8 @@ const page = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="h-5 w-5 text-primary">
+              className="h-5 w-5 text-primary"
+            >
               <path
                 fillRule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -83,7 +85,8 @@ const page = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="h-5 w-5">
+              className="text-primary h-5 w-5"
+            >
               <path
                 fillRule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -92,8 +95,15 @@ const page = () => {
             </svg>
           </div>
           <div className="timeline-end mb-10">
-            <time className="font-mono italic">2023.10 ~ 재직중</time>
+            <time className="font-mono italic">2023.10 ~ 2025.01</time>
             <div className="text-lg font-black">CPLABS corp. 프로</div>
+            <p>1. 컨트랙트 연동 BackOffice 담당(Next.js App router)</p>
+            <p>2. 공지사항 페이지 담당(Next.js Pages router) - markdown</p>
+            <p>3. React 15 ver. Class Component 유지보수</p>
+            <p>
+              4. SEO처리(Next.js Pages router) - SEO Component 생성 - SEO
+              모니터링 & 디버깅
+            </p>
           </div>
         </li>
       </ul>
