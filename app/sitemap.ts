c23338a9c7af4a54acad9f32710be7137b2cee22
gogleset <choij2494@gmail.com>
2025-01-28 10:55:03 +0900

    sitemap 주석

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,13 +5,30 @@ import type { Sitemap } from "./types/sitemaps";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const dynamicSitemap: Sitemap[] = (await frontmatters()).map(
-    (frontmatter) => ({
-      url: `${BASE_URL}/posts/${frontmatter.filename}`,
-      lastModified: frontmatter.date,
-      changeFrequency: frontmatter.sitemap.changefreq,
-      priority: frontmatter.sitemap.priority,
-    })
+    /**
+   * 
+   * @param frontmatter ---
+title: ""
+description: ""
+date: ""
+tag: []
+filename: ""
+sitemap:
+  changefreq: # 페이지 업데이트 빈도
+  priority: # 검색엔진 우선순위 (0.0 ~ 1.0)
+---
+   */
+    (frontmatter) => {
+      console.log(frontmatter);
+      return {
+        url: `${BASE_URL}/posts/${frontmatter.filename}`,
+        lastModified: frontmatter.date,
+        changeFrequency: frontmatter.sitemap.changefreq,
+        priority: frontmatter.sitemap.priority,
+      };
+    }
   );
+
   const staticSitemap: Sitemap[] = [
     {
       url: BASE_URL,
